a0f26d01abf344742570b6ab35b992bdc3859482
ice <1413795920@qq.com>
2024-11-28 20:46:45 +0800

    修改主题名称1

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
         <a href="https://vuejs.org/" target="_blank">
             <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
         </a>
-        <h1>CICD/成功自动化部署</h1>
+        <h1>成功自动化部署😋🫵🫵</h1>
         <chartPie :series-data="dataList" :extra-option="extraOption" />
     </div>
     <!-- <HelloWorld msg="Vite + Vue" /> -->
